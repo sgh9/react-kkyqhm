505088bdb1b4a59208dfb6dd91edb13f1374a1f1
sgh9 <hiremath280997@gmail.com>
2021-09-07 13:45:51 +0530

    2 files modified

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Modal = ({ children }) => {
   const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  });
   return (
-    <div className="modal-container">
+    <div className={`modal-container ${show}`}>
       <div className="modal">{children}</div>
     </div>
   );
